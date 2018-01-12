21b32974c3e8ab2ae1c431c6c0409118eca1e150
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-12 17:46:59 -0500

    added more files

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     //Create an instance of the Base Class
     $f3 = Base :: instance();
 
-    //Define a default rout
+    //Set debug level
+    //will take care of php errors as well which gives 500 error
+    $f3->set('DEBUG', 3);
+
+    //Define a default route
     $f3->route('GET /', function ()
     {
         $view = new View;
@@ -15,5 +19,24 @@
     }
     );
 
-    //Run fat free
+
+    //this will be shown if user goes to url ..328/routing-demo/page1
+    $f3->route('GET /page1', function ()
+    {
+        $view = new View;
+        echo '<h1>This is your page 1</h1>';
+    }
+    );
+
+
+    //this will be shown if user goes to url ..328/routing-demo/page2/subpage-a
+    $f3->route('GET /page2/subpage-a', function ()
+    {
+        $view = new View;
+        echo '<h1>This is your page 2, subpage-a</h1>';
+    }
+    );
+
+
+//Run fat free
     $f3->run();
